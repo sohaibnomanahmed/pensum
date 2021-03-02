3496c00a8a604a41e96c2ae5606ca95d8569369c
Sohaib Ahmed <sohaib.n@icloud.com>
2021-03-02 19:56:40 +0100

    add emulator

--- lib/global/services.dart
+++ lib/global/services.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:leaf/messages/recipients_service.dart';
 
 import '../messages/messages_service.dart';
@@ -14,10 +15,21 @@ import 'image_picker_service.dart';
 import 'image_upload_service.dart';
 
 class FirebaseService {
+  // Switch host based on platform.
+  static final String host = defaultTargetPlatform == TargetPlatform.android
+      ? '10.0.2.2:8080'
+      : 'localhost:8080';
+
   // init firebase instances
   static final firebaseAuth = FirebaseAuth.instance;
   static final firestore = FirebaseFirestore.instance;
   static final firebaseStorage = FirebaseStorage.instance;
+  
+  // setup local developement environment
+  static final firestoreEmulator = firestore.settings  =
+    Settings(host: host, sslEnabled: false);
+  static final authEmulator = firebaseAuth.useEmulator('http://localhost:9099');  
+
   // init firebase services
   static final authentication = AuthenticationService(firebaseAuth);
   static final profile = ProfileService(firestore);
@@ -29,7 +41,7 @@ class FirebaseService {
   static final imageUpload = ImageUploadService(firebaseStorage);
 }
 
-class NativeService{
+class NativeService {
   // init native services
   static final imagePicker = ImagePickerService();
   static final imageCropper = ImageCropperService();
