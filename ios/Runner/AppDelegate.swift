83c347a4494526db0a2b2f348eb3bec0bf51ae3a
Sohaib Ahmed <sohaib.noman>
2021-05-21 17:18:05 +0200

    Add launcher and splash icons.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,9 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("YAIzaSyASyQrD1FCJEmrPvrcLUWeGcn2pubkqFy8")
+    var flutter_native_splash = 1
+    UIApplication.shared.isStatusBarHidden = false
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
