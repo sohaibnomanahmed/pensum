a50abaaa6caab9cf8243228ce06a039a8c25f909
Sohaib Ahmed <sohaib.noman>
2021-06-09 23:21:53 +0200

    Add commercial data users, profile images and sort user deals.

--- lib/profile/widgets/profile_deals_list.dart
+++ lib/profile/widgets/profile_deals_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:leaf/deals/models/deal.dart';
 //import 'package:leaf/deals/deals_page.dart';
 import 'package:provider/provider.dart';
 
@@ -9,6 +10,7 @@ class ProfileDealsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final profileDeals = context.watch<ProfileProvider>().profile!.deals;
+    profileDeals.sort((a, b) => Deal.convertPriceToInt(a.price).compareTo(Deal.convertPriceToInt(b.price)));
     // list view puts on top padding if not app bar
     // https://github.com/flutter/flutter/issues/14842
     return MediaQuery.removePadding(
@@ -18,32 +20,7 @@ class ProfileDealsList extends StatelessWidget {
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         separatorBuilder: (_, __) => Divider(),
-        itemBuilder: (_, index) => InkWell(
-          onTap: () async {
-            // final book = await context
-            //     .read<ProfileProvider>()
-            //     .getDealedBook(profileDeals[index].pid);
-            // if (book != null) {
-            //   // navigate to books page
-            //   await Navigator.of(context).pushNamed(
-            //     DealsPage.routeName,
-            //     arguments: book,
-            //   );
-            // } else {
-            //   // show error message
-            //   final scaffoldMessenger = ScaffoldMessenger.of(context);
-            //   // remove snackbar if existing and show a new with error message
-            //   scaffoldMessenger.hideCurrentSnackBar();
-            //   scaffoldMessenger.showSnackBar(
-            //     SnackBar(
-            //       backgroundColor: Theme.of(context).errorColor,
-            //       content: Text('Something went wrong, please try again!'),
-            //     ),
-            //   );
-            // }
-          },
-          child: ProfileDealItem(key: ValueKey(profileDeals[index].pid), deal: profileDeals[index]),
-        ),
+        itemBuilder: (_, index) => ProfileDealItem(key: ValueKey(profileDeals[index].pid), deal: profileDeals[index]),
         itemCount: profileDeals.length,
       ),
     );
